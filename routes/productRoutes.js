00ddd60dd20ffa232750fff9a5ebe43ec60a4fac
Bunyodbek1505 <jorayevbunyodbek1505@gmail.com>
2024-09-21 00:39:55 -1100

    added crud products

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,12 +1,32 @@
-import express from 'express';
+import express from 'express'
+import formidable from 'express-formidable'
+import { createProductController, deleteProductController, getProductController, getSingleProductController, updateProductController } from '../controllers/productController.js'
 import { isAdmin, requireSignIn } from '../middlewares/authMiddleware.js'
-import { createProductController } from '../controllers/productController.js'
+
 
 
 const router = express.Router()
 
 // routes 
-router.post('/create-product', requireSignIn, isAdmin, createProductController)
+router.post('/create-product', requireSignIn, isAdmin, formidable(), createProductController)
+
+// get products
+router.get('/get-product', getProductController)
+
+// single product
+router.get('/get-product/:slug', getSingleProductController)
+
+// update product
+router.put(
+	"/update-product/:pid",
+	requireSignIn,
+	isAdmin,
+	formidable(),
+	updateProductController
+)
+
+// delete product
+router.delete('/product/:pid', deleteProductController)
 
 
 export default router
